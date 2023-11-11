2dddf5ce66ea61aa790c5484a31dbac4ca69b865
taogawa <taogwjp@gmail.com>
2023-11-12 08:12:23 +0900

    Fix CI broken

--- test/helper_test.rb
+++ test/helper_test.rb
@@ -14,11 +14,11 @@ class HelperTest < ActionView::TestCase
   end
 
   def test_stylesheet_pack_tag
-    assert_equal '<link rel="stylesheet" href="/packs/css/application-12345.css" media="screen" />', stylesheet_pack_tag("application.css")
+    assert_match /<link\s+rel="stylesheet"(?=.*href="\/packs\/css\/application-\d+\.css")(?=.*media="screen")[^>]*\s+\/>/, stylesheet_pack_tag("application")
   end
 
   def test_stylesheet_pack_tag_without_extname
-    assert_equal '<link rel="stylesheet" href="/packs/css/application-12345.css" media="screen" />', stylesheet_pack_tag("application")
+    assert_match /<link\s+rel="stylesheet"(?=.*href="\/packs\/css\/application-\d+\.css")(?=.*media="screen")[^>]*\s+\/>/, stylesheet_pack_tag("application")
   end
 
   def test_asset_pack_path
